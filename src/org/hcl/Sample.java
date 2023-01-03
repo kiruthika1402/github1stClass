50a43343b0b86b6c43aff337a8252d5a8555daea
kiruthika1402 <kiruthikalaxmi1402@gmail.com>
2023-01-03 13:00:18 +0530

    my task

---
+++ src/org/hcl/Sample.java
@@ -0,0 +1,14 @@
+package org.hcl;
+
+public class Sample {
+
+	public static void main(String[] args) {
+         
+		System.out.println(1);
+		System.out.println(2);
+		System.out.println(3);
+		System.out.println(4);
+		System.out.println(5);
+	}
+
+}
